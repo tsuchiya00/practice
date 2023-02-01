d67c7051dca3090ca334fd94da2ed09a2419ac74
tsuchiya00 <124007698+tsuchiya00@users.noreply.github.com>
2023-02-01 09:35:41 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,15 @@
+public abtract class TangibleAsset{
+  private String name;
+  Private int price
+  private String color;
+  
+  public TanggibleAsset(String name, int price, String color){
+    this.name = name;
+    this.price = price;
+    this.color = color;
+  }
+  
+  public String getName(){return this.name}
+  public int getPrice(){return this.price}
+  public String getColor(){return this.color}
+}
